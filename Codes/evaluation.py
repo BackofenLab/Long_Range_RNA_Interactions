e941fc1354a42c8255e6a56818c848dbc1cf458f
MatthiasHerrmann <herrmannmatthias@gmx.net>
2023-11-08 17:34:24 +0100

    Multiple subopts and 3. lineplot

--- Codes/evaluation.py
+++ Codes/evaluation.py
@@ -14,12 +14,13 @@ def draw_lineplots(df, extra_bases_roi, output):
     extra_bases_roi (int): Amount of extra bases from the CDS used on each side
     output (str): Filepath where the plot should be saved
     """
-    ## Need 2 plots:
+    ## Need 3 plots:
     ## 1: UTR5+some_CDS+t_inter
     ## 2: some_CDS+UTR3+q_inter
+    ## 3: Like 2 but aligned to the genome end (end of UTR3)
     ## inter can be either on UTR or CDS (or maybe overlapping both)
     plt.style.use("seaborn-darkgrid")
-    fig, (side5, side3) = plt.subplots(2, figsize=(16, 9))
+    fig, (side5, side3, side3_re) = plt.subplots(3, figsize=(16, 15))
 
     for index, row in df.iterrows():
         #if row["id"] != "NC_001437.1":
@@ -31,13 +32,15 @@ def draw_lineplots(df, extra_bases_roi, output):
         line3sub = []
         for suboptt in ast.literal_eval(row["suboptts"]): ## Subopt stuff..
             if suboptt:
-                line5sub.append(((suboptt[0][0], index),
-                              (suboptt[0][1], index))) ## orange?
+                for subopt in suboptt:
+                    line5sub.append(((subopt[0], index),
+                              (subopt[1], index))) ## orange?
         subopt_len5 = len(line5sub)
         for suboptq in ast.literal_eval(row["suboptqs"]): ## Subopt stuff..
             if suboptq:
-                line3sub.append(((suboptq[0][0], index),
-                              (suboptq[0][1], index))) ## orange?
+                for subopt in suboptq:
+                    line3sub.append(((subopt[0], index),
+                                     (subopt[1], index))) ## orange?
         subopt_len3 = len(line3sub)
         line5 = [((0, index), (extra_bases_roi, index)), ## grey
                  ((0, index), (-row["UTR5len"], index))] ## blue
@@ -49,25 +52,34 @@ def draw_lineplots(df, extra_bases_roi, output):
         line3 += line3sub
         line3 += [((q_tuple[0], index),
                  (q_tuple[1], index))] ## red
-        CDS_colors = {"ISFV":"lime","MBFV":"b","NKV":"g","TBFV":"m"}
-        colors = [CDS_colors[row["class"]], "c"] + ["orange"]*subopt_len3 + ["r"]
-        side5.add_collection(LineCollection(line5, colors=colors, linewidths=(2,)))
-        side3.add_collection(LineCollection(line3, colors=colors, linewidths=(2,)))
+        lin3_realigned = []
+        for range_tuple in line3:
+            lin3_realigned.append(((range_tuple[0][0] - row["UTR3len"], index), (range_tuple[1][0] - row["UTR3len"], index)))
+        CDS_colors = {"ISFV":"c","MBFV":"b","NKV":"g","TBFV":"m"}
+        colors5 = [CDS_colors[row["class"]], "gray"] + ["orange"]*subopt_len5 + ["r"]
+        colors3 = [CDS_colors[row["class"]], "gray"] + ["orange"]*subopt_len3 + ["r"]
+        side5.add_collection(LineCollection(line5, colors=colors5, linewidths=(2,)))
+        side3.add_collection(LineCollection(line3, colors=colors3, linewidths=(2,)))
+        side3_re.add_collection(LineCollection(lin3_realigned, colors=colors3, linewidths=(2,)))
     side5.set_xlabel("Distance from 5'UTR-CDS transition")
     side3.set_xlabel("Distance from CDS-3'UTR transition")
+    side3_re.set_xlabel("Distance from 3'UTR end")
     side5.set_ylabel("Index")
     side3.set_ylabel("Index")
+    side3_re.set_ylabel("Index")
     side5.autoscale_view()
     side3.autoscale_view()
+    side3_re.autoscale_view()
     legend_elements = [Line2D([0], [0], color=CDS_colors["ISFV"], lw=4, label='ISFV-CDS'),
                        Line2D([0], [0], color=CDS_colors["MBFV"], lw=4, label='MBFV-CDS'),
                        Line2D([0], [0], color=CDS_colors["NKV"], lw=4, label='NKV-CDS'),
                        Line2D([0], [0], color=CDS_colors["TBFV"], lw=4, label='TBFV-CDS'),
-                       Line2D([0], [0], color='c', lw=4, label='UTR'),
+                       Line2D([0], [0], color='gray', lw=4, label='UTR'),
                        Line2D([0], [0], color='r', lw=4, label='Interaction'),
                        Line2D([0], [0], color='orange', lw=4, label='Subopt')]
     side5.legend(handles=legend_elements,loc="upper left")
     side3.legend(handles=legend_elements,loc="upper right")
+    side3_re.legend(handles=legend_elements,loc="upper left")
     plt.suptitle("Interaction Lineplot")
     plt.savefig(output)
     plt.close()
