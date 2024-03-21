a690efaad65ba072adf08d58b7a30b71437c45c9
MatthiasHerrmann <herrmannmatthias@gmx.net>
2024-03-21 12:41:08 +0100

    Updated Sequence Logos

--- Codes/make_motif.py
+++ Codes/make_motif.py
@@ -34,7 +34,9 @@ def fasta_to_df(fasta, reverse_sequences=False, drop_minus=False):
 
 def add_interactions(motif, motif2, inter_left, inter_right, color="cornflowerblue",
                      vertical=False, dynamic_alpha=True):
-    alpha = 1
+    alpha = alpha2 = 1
+    linewidth = 8
+    alt_bracket_color = "firebrick"
     if vertical:
         arc_rad = 0
         height_left = -0.15
@@ -55,6 +57,7 @@ def add_interactions(motif, motif2, inter_left, inter_right, color="cornflowerbl
         raise(ValueError("Interactions need to have the same lengths as their sequences"))
     l_pos = 0
     r_pos = 0
+    inter_count = 0
     while l_pos < len(motif.df) and r_pos < len(motif2.df):
         if vertical:
             xyB_pos = r_pos
@@ -66,17 +69,27 @@ def add_interactions(motif, motif2, inter_left, inter_right, color="cornflowerbl
         elif inter_right[r_pos] != ")" and inter_right[r_pos] != "]":
             r_pos += 1
         elif inter_left[l_pos] == "(" and inter_right[r_pos] == ")":
+            inter_count += 1
+            tmp_color = color
+            if inter_count % 5 == 0:
+                tmp_color = "black"
             con = ConnectionPatch(xyA=(l_pos, height_left), xyB=(xyB_pos, height_right), coordsA=motif.ax.transData, coordsB=motif2.ax.transData,
-                  axesA=motif, axesB=motif2, connectionstyle=f"arc3,rad={arc_rad}",color=color, alpha=alpha, linewidth=4)
+                  axesA=motif, axesB=motif2, connectionstyle=f"arc3,rad={arc_rad}",color=tmp_color, alpha=alpha, linewidth=linewidth)
             motif2.ax.add_artist(con)
             if dynamic_alpha:
                 alpha -= (1/(left_brackets_1+3))
             l_pos += 1
             r_pos += 1
         elif inter_left[l_pos] == "[" and inter_right[r_pos] == "]":
+            inter_count += 1
+            tmp_color = alt_bracket_color
+            if inter_count % 5 == 0:
+                tmp_color = "black"
             con = ConnectionPatch(xyA=(l_pos, height_left), xyB=(xyB_pos, height_right), coordsA=motif.ax.transData, coordsB=motif2.ax.transData,
-                  axesA=motif, axesB=motif2, connectionstyle=f"arc3,rad={arc_rad}", color="firebrick", linewidth=4)
+                  axesA=motif, axesB=motif2, connectionstyle=f"arc3,rad={arc_rad}", color=tmp_color, alpha=alpha2, linewidth=linewidth)
             motif2.ax.add_artist(con)
+            if dynamic_alpha:
+                alpha2 -= (1/(left_brackets_2+3))
             l_pos += 1
             r_pos += 1
         else:
@@ -84,7 +97,8 @@ def add_interactions(motif, motif2, inter_left, inter_right, color="cornflowerbl
 
 
 def create_motif(input_fasta_left, input_fasta_right, output_file="", 
-                 drop_minus=True, color="cornflowerblue", vertical=False, dynamic_alpha=True):
+                 drop_minus=True, color="cornflowerblue", vertical=False, dynamic_alpha=True,
+                 plot_xaxis=False, plot_yaxis=False):
     fasta_df_left, inter_left = fasta_to_df(input_fasta_left, drop_minus=drop_minus)
     if vertical:
         plt.figure(figsize=(10, 5))
@@ -126,13 +140,23 @@ def create_motif(input_fasta_left, input_fasta_right, output_file="",
     motif2.ax.xaxis.set_ticks_position('none')
     motif.ax.xaxis.set_tick_params(pad=-1)
     motif2.ax.xaxis.set_tick_params(pad=-1)
-    motif.ax.set_yticks([0.2, 0.4, 0.6, 0.8, 1])
-    motif2.ax.set_yticks([0.2, 0.4, 0.6, 0.8, 1])
+    #motif.ax.tick_params(axis="x", labelsize=24)
+    #motif2.ax.tick_params(axis="x", labelsize=24)
+    #motif.ax.set_yticks([0.2, 0.4, 0.6, 0.8, 1])
+    #motif2.ax.set_yticks([0.2, 0.4, 0.6, 0.8, 1])
     if vertical:
         plt.subplots_adjust(hspace=0.5, bottom=0.05, top=0.99, left=0.05, right=0.99)
     else:
         #plt.tight_layout()
         plt.subplots_adjust(wspace=0.05, bottom=0.05, top=0.4, left=0.01, right=0.99)
+    if not plot_xaxis:
+        motif.ax.get_xaxis().set_visible(False)
+        motif2.ax.get_xaxis().set_visible(False)
+        plt.subplots_adjust(bottom=0.01)
+    if not plot_yaxis:
+        motif.ax.get_yaxis().set_visible(False)
+        motif2.ax.get_yaxis().set_visible(False)
+        plt.subplots_adjust(left=0.01)
 
     # style and show figure
     if not output_file:
@@ -143,9 +167,6 @@ def create_motif(input_fasta_left, input_fasta_right, output_file="",
 
 
 if __name__ == "__main__":
-    #colours = {"blue":"cornflowerblue",
-    #           "pink":"violet",
-    #           "yellow":"orange"}
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", dest="directory",
                         type=str, help="Directory with fasta files", required=True)
@@ -156,8 +177,6 @@ if __name__ == "__main__":
                 valid_file_left = f"{root}/{file}"
                 valid_file_right = valid_file_left.replace("_left.fa", "_right.fa")
                 color = valid_file_left.split("_")[1]
-                #print(color, colours[color])
-                #color = colours[color]
                 for vertical in [True, False]:
                     if vertical:
                         orientation = "vertical"
@@ -166,4 +185,4 @@ if __name__ == "__main__":
                     output_name = valid_file_left.replace("_left.fa", f"_{orientation}.png")
                     create_motif(valid_file_left, valid_file_right, 
                                  output_file=output_name, color=color, 
-                                 vertical=vertical, dynamic_alpha=True)
+                                 vertical=vertical, dynamic_alpha=False)
